930f9b151847a6df074e3d29a7b0128a8460cab5
Paulo Almeida <palmeida@kenzanmedia.com>
2015-06-17 15:38:56 -0300

    tunnel feature

--- configLoader.py
+++ configLoader.py
@@ -11,28 +11,26 @@ class ConfigLoader:
 		"button1"
 	]
 
-	credentials = {
-		'sshUsername': None,
-		'sshPublicKey': None
-	}
+	preferences = None;
 
 	@staticmethod
 	def configure(builder, filePath):
 		print "Loading configuration from file [" + filePath + "]..."
 
 		with open(filePath) as config_file:
-			configDictionary = json.loads(config_file.read())
+			ConfigLoader.preferences = json.loads(config_file.read())
 
 		for widget_id in ConfigLoader.widgets:
 			widget = builder.get_object(widget_id)
-			widget.set_label(configDictionary[widget_id])
+			widget.set_label(ConfigLoader.preferences[widget_id])
 
 		#config Tunnel selectCombo
 		tunnelSelectCombo = builder.get_object("selectCombo1")
 		model = Gtk.ListStore(str)
-		for entry in configDictionary["tunnelList"]:
+		for entry in ConfigLoader.preferences["tunnelList"]:
 			model.append([entry["label"]])
 		tunnelSelectCombo.set_model(model)
 		renderer_text = Gtk.CellRendererText()
 		tunnelSelectCombo.pack_start(renderer_text, True)
 		tunnelSelectCombo.add_attribute(renderer_text, "text", 0)
+
--- widgetHandlers.py
+++ widgetHandlers.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+from configLoader import ConfigLoader
+import subprocess
+
 class WidgetHandlers:
 
 	def __init__(self, builder):
 		self.builder = builder
+		self.sshProcess = None
 
 	def onToggleButton1_clicked(self, widget):
 		print "ToggleButton1 Clicked"
@@ -13,9 +17,24 @@ class WidgetHandlers:
 
 	def onToggleButton3_clicked(self, widget):
 		selectCombo1 = self.builder.get_object("selectCombo1")
+
 		if widget.get_active():
 			selectCombo1.set_sensitive(False)
+
+			user   = ConfigLoader.preferences['credentials']['sshUsername']
+			sshKey = ConfigLoader.preferences['credentials']['sshPrivateKey']
+
+			command = ''
+			for target in ConfigLoader.preferences['tunnelList'][0]['path']:
+				command = command + ' -L '  + target['originPort'] + ':' + target['targetHost'] + ':' + target['destPort']
+
+			self.sshProcess = subprocess.Popen('ssh -i ' + sshKey + ' ' + user + command, shell=True)
 		else:
+
+			if self.sshProcess is not None:
+				print "Killing process..."
+				self.sshProcess.terminate()
+				self.sshProcess = None
 			selectCombo1.set_sensitive(True)
 
 	def onButton1_clicked(self, widget):
